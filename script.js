f53b2a3a4ca15089a40f5a950e45bf27470e96a8
Jamie Flynt <jamflynt@gmail.com>
2022-11-09 09:52:08 -0500

    Added WOO

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function randomNum(){
   if (ranNum === 1){
     alert("You have rolled a 1, It's not your day is it?")
   } else {
-    alert(`You rolled a ${ranNum} congrats something happened`);
+    alert(`You rolled a ${ranNum} congrats something happened WOO!`);
   };
 };
